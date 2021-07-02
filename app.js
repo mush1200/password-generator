f23421b28c5dc44b6cc29de425dcc0dbc534db74
mush1200 <mush120@hotmail.com>
2021-07-02 15:25:02 +0800

    取得表單資料

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+
+app.use(express.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.render('index')
+})
 app.listen(port, () => {
   console.log(`The Express server is running on http://localhost:${port}.`)
 })
